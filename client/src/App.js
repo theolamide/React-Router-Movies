811a3026faa0e9e0af05cbf2eebfdce68e0fbdb0
Daniel Ajadi <ajadidan@gmail.com>
2019-10-16 15:47:56 -0400

    Initial Commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
 
 import SavedList from './Movies/SavedList';
+import { Route } from 'react-router-dom';
+import MovieList from "./Movies/MovieList";
+
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
+  const [movieList] = useState ([]);
 
   const addToSavedList = movie => {
     setSavedList( [...savedList, movie] );
@@ -12,9 +16,26 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route exact 
+            path = "/"
+            component = {MovieList} 
+            // render = {props => (
+            //   <MovieList 
+            //   {...props}
+              // movies = {movieList} 
+              
+              // />
+            // )}    
+          />
+
+      <Route 
+          path = "Movie/:id"
+          render = {props => <MovieList {...props} movies = {movieList} />}
+          />
+
     </div>
   );
 };
 
 export default App;
+ 
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -7,8 +7,9 @@ const MovieList = props => {
     const getMovies = () => {
       axios
         .get('http://localhost:5000/api/movies')
-        .then(response => {
-          setMovies(response.data);
+        .then(response => {         
+          console.log(response.data);
+           setMovies(response.data);
         })
         .catch(error => {
           console.error('Server Error', error);
